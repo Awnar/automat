92df567a4bb331dae970e46637fb58839fe277a0
Maciej Dembiński <awnar@interia.pl>
2019-02-17 13:05:38 +0100

    fix

--- automat/Program.cs
+++ automat/Program.cs
@@ -10,9 +10,14 @@ namespace automat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj ilość stanów");
-            var input = Console.ReadLine();
-            int n = int.Parse(input);
+            string input;
+            int n;
+            do
+            {
+                Console.WriteLine("Podaj ilość stanów");
+                input = Console.ReadLine();
+                n = int.Parse(input);
+            } while (n < 1);
 
             int fs;
             do
@@ -20,16 +25,23 @@ namespace automat
                 Console.WriteLine("Podaj ilość stanów akceptujących (końcowych)");
                 input = Console.ReadLine();
                 fs = int.Parse(input);
-            } while (fs > n);
+            } while (fs > n || fs < 1);
 
-            Console.WriteLine("Podaj ilość znaków w alfabecie");
-            input = Console.ReadLine();
-            int al = int.Parse(input);
+            int al;
+            do
+            {
+                Console.WriteLine("Podaj ilość znaków w alfabecie");
+                input = Console.ReadLine();
+                al = int.Parse(input);
+            } while (al < 1);
 
-            Console.WriteLine("Szansa na przejście do innego stanu, 1/X");
-            input = Console.ReadLine();
-            int X = int.Parse(input);
-            X *= n;
+            double X;
+            do
+            {
+                Console.WriteLine("Szansa na przejście do innego stanu, 1>=X>=0");
+                input = Console.ReadLine();
+                X = double.Parse(input);
+            } while (0 > X || X > 1);
 
             //tablica z stanami przejść 
             int[,] tab = new int[n, al + 1];
@@ -52,14 +64,16 @@ namespace automat
             {
                 for (int j = 0; j < n; j++)
                 {
-                    tab[j, i] = rand.Next(X);
+                    if (rand.NextDouble() <= X)
+                        tab[j, i] = rand.Next(n);
+                    else tab[j, i] = n + 1;
                 }
             }
 
             //wyświetlanie tabeli
             Console.Write("\nStany\tfinal\t");
             for (int i = 'a'; i < 'a' + al; i++)
-                Console.Write((char)i + "\t");
+                Console.Write((char) i + "\t");
             Console.WriteLine();
             for (int i = 0; i < n; i++)
             {
@@ -71,6 +85,7 @@ namespace automat
                     else
                         Console.Write("-\t");
                 }
+
                 Console.WriteLine();
             }
 
@@ -86,7 +101,8 @@ namespace automat
             {
                 var start = 0;
                 do
-                { //losowanie stanu końcowego
+                {
+                    //losowanie stanu końcowego
                     start = rand.Next(n);
                 } while (tab[start, 0] != 1);
 
@@ -94,21 +110,42 @@ namespace automat
                 {
                     var next = 0;
                     bool t = true;
+                    int tmp = 0;
                     do
-                    {//losowanie znaku z alfabetu i sprawdzanie czy jest jakieś przejście do obecnego stanu
+                    {
+                        //losowanie znaku z alfabetu i sprawdzanie czy jest jakieś przejście do obecnego stanu
                         next = rand.Next(al) + 1;
-                        for (int j = 0; j < n; j++)
-                            if (tab[j, next] == start)
-                            {
-                                start = j;
-                                t = false;
-                                break;
-                            }
+                        if (rand.Next(1) == 1)
+                        {
+                            for (int j = 0; j < n; j++)
+                                if (tab[j, next] == start)
+                                {
+                                    start = j;
+                                    t = false;
+                                    break;
+                                }
+                        }
+                        else
+                        {
+                            for (int j = n-1; j >= 0; j--)
+                                if (tab[j, next] == start)
+                                {
+                                    start = j;
+                                    t = false;
+                                    break;
+                                }
+                        }
+
+                        if (tmp++ > 2 * n)//jeśli nie wylosuje pasującego przejścia reset słowa
+                        {
+                            next = 0;
+                            tmp = 0;
+                        }
                     } while (t);
+
                     //uzupełnianie słowa
-                    slowa[i] = (char)('a' + next - 1) + slowa[i];
-                    var x = slowa[i];
-                } while (start != 0);//półki nie jest to stan 0 (startowy)
+                    slowa[i] = (char) ('a' + next - 1) + slowa[i];
+                } while (start != 0); //półki nie jest to stan 0 (startowy)
             }
 
             //wyświetlanie słów
